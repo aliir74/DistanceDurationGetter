a95e51ccec35c4605d3516c011b7384d6aad2b8e
aliir74 <aliirani74@gmail.com>
2018-01-10 18:22:05 +0330

    use googlemaps library and call distance_matrix function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import googlemaps
+
+apikey = open('key.txt', 'r').readline()
+
+gmaps = googlemaps.Client(key=apikey)
+
+l = [float(i) for i in input().split()]
+ret = gmaps.distance_matrix((l[0], l[1]), (l[2], l[3]))
+
+print(ret)
