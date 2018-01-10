b9804dce5e259748797fa866cf6ab81d488bb966
aliir74 <aliirani74@gmail.com>
2018-01-10 19:08:25 +0330

    add writeResultToCSV for write result to csv file

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import googlemaps
+import csv
+
+def writeResultToCSV(file, result):
+    csvwriter = csv.writer(file)
+
+    #write headers
+    header = ['origins/destinations']+result['destination_addresses']
+    csvwriter.writerow(header)
+
+    #add content
+    for (idx, i) in enumerate(result['origin_addresses']):
+        row = [i]
+        for j in result['rows'][idx]['elements']:
+            row.append(j['duration']['text'])
+
+        csvwriter.writerow(row)
+
 
 def getDataFromFile(file):
     origins = []
@@ -38,7 +55,10 @@ for i in origins:
 print('dests:')
 for i in dests:
     print(i)
-ret = gmaps.distance_matrix(origins[:10], dests[:])
+ret = gmaps.distance_matrix(origins[:20], dests[:3])
 
+resultCSV = open('result.csv', 'w')
+writeResultToCSV(resultCSV, ret)
+#print(ret['rows'][0]['elements'][0]['duration'])
 
-print(ret)
+#print(ret)
