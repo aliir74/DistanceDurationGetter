9bd7bdc80146c6c8090829707ab32c4a4f778f2a
aliir74 <aliirani74@gmail.com>
2018-01-10 18:30:32 +0330

    add getDataFromFile function

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import googlemaps
 
+def getDataFromFile(file):
+    origins = []
+    dests = []
+    ob = False
+    od = False
+    for line in file:
+        if("origin" in line):
+            oo = True
+            od = False
+        elif("destination" in line):
+            ob = True
+            od = False
+        else:
+            if(od):
+                location = (float(i) for i in line.split())
+                dests.append(location)
+            elif(ob):
+                location = (float(i) for i in line.split())
+                origins.append(location)
+    return origins, dests
+
+
 apikey = open('key.txt', 'r').readline()
 
 gmaps = googlemaps.Client(key=apikey)
 
-l = [float(i) for i in input().split()]
 ret = gmaps.distance_matrix((l[0], l[1]), (l[2], l[3]))
 
 print(ret)
