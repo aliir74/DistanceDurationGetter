ec45d3416a4b2d86eab4a9f9adbce4decaa6193d
aliir74 <aliirani74@gmail.com>
2018-01-13 16:08:57 +0330

    get duration_in_traffic (current traffic) instead of average duration

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ def writeResultToCSV(file, result1, result2):
     for (idx, i) in enumerate(result1['origin_addresses']):
         row = [idx+1]
         for j in (result1['rows'][idx]['elements']+result2['rows'][idx]['elements']):
-            row.append(j['duration']['value'])
+            if(j['status'] == 'ZERO_RESULTS'):
+                print(j)
+                row.append(0)
+            else:
+                row.append(j['duration_in_traffic']['value'])
 
         csvwriter.writerow(row)
 
@@ -53,6 +57,10 @@ for i in dests:
     print(i)
 
 resultCSV = open('result.csv', 'w')
-ret1 = gmaps.distance_matrix(origins, dests[:25])
-ret2 = gmaps.distance_matrix(origins, dests[25:])
+ret1 = gmaps.distance_matrix(origins, dests[:25], mode="driving", traffic_model="best_guess", departure_time="now")
+print('result 1 finished')
+ret2 = gmaps.distance_matrix(origins, dests[25:], mode="driving", traffic_model="best_guess", departure_time="now")
+print('result 2 finished')
+
 writeResultToCSV(resultCSV, ret1, ret2)
+print('merge finished!')
